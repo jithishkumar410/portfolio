48d9e93f94515418d5efbf88a2d5215cd52f0293
jithishkumar410 <97963977+jithishkumar410@users.noreply.github.com>
2022-08-11 10:10:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let a=document.querySelector(".tog");
+let b=document.querySelector(".nul")
+let c=document.querySelector(".to")
+let d=document.querySelector(".x")
+a.addEventListener("click",()=>{
+    b.classList.toggle("sl");
+});
+
